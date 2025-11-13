e3a317c4d7616eccb82872170a0960a769d99e59
egemenakdan <egemenvkdvn35@gmail.com>
2025-11-13 13:57:05 +0100

    Add project files.

---
+++ SearchingAnalysis/SearchingAnalysis.cpp
@@ -0,0 +1,12 @@
+﻿// SearchingAnalysis.cpp : Defines the entry point for the application.
+//
+
+#include "SearchingAnalysis.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
